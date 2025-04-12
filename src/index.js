024c33a32b3e7f2469e5a9003529f530097df731
quang1404 <86954780+quang1404@users.noreply.github.com>
2025-04-12 19:41:29 +0700

    cap nhat 12/4

--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
-const  handlebars = require('express-handlebars');
 const express = require('express');
+const { engine } = require('express-handlebars');
 const morgan = require('morgan');
+const path = require('path');
+
 const app = express();
 const port = 3000;
-//HTTP logger
-app.use(morgan('combined'));
-//template engine
-app.engine('handlebars', handlebars());
-app.set('view engine', 'handlebars');
 
+app.use(express.static(path.join(__dirname, 'public')))
+
+// HTTP logger
+app.use(morgan('combined'));
+
+// Template engine
+app.engine('hbs', engine({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+// Home route
 app.get('/', (req, res) => {
   res.render('home');
-})
+});
+
+app.get('/news', (req, res) => {
+  res.render('news');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-})
+});
