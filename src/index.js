6f60008296fde11124b960387ff1b063f9a8039f
quang1404 <ndq14042002@gmail.com>
2025-06-13 22:24:02 +0700

    Up router and controller

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
+const ip = require('ip');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
 const express = require('express');
 const { engine } = require('express-handlebars');
 const morgan = require('morgan');
+
 const path = require('path');
-
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3001; // Change 3000 to 3001 or another free port
 
+const route = require('./routes');
+
+app.use(cors({ origin: '*' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 // Template engine
 app.engine('hbs', engine({
@@ -18,14 +28,8 @@ app.engine('hbs', engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-// Home route
-app.get('/', (req, res) => {
-  res.render('home');
-});
-
-app.get('/news', (req, res) => {
-  res.render('news');
-});
+//Routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
